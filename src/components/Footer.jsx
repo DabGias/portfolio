53677869fedcb4a420f75b46c23a88605339e2d4
Logon Aluno <logonrmlocal@fiap.com.br>
2023-02-10 11:10:16 -0300

    melhoramentos na home

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,48 @@
+import styled from 'styled-components'
+
+const StyleFooter = styled.footer `
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    color: #d9d9d9;
+    padding: 10px;
+
+    h1 {
+        color: #ecec31;
+        margin-bottom: 40px;
+    }
+
+    p {
+        margin-bottom: 20px;
+    }
+
+    p span {
+        color: #ecec31;
+    }
+
+    a {
+        display: block;
+        text-decoration: none;
+        color: #d9d9d9;
+        margin-bottom: 20px;
+        transition: 150ms;
+    }
+
+    a:hover {
+        color: #ecec31;
+        transition: 150ms;
+    }
+`
+
 function Footer() {
     return(
         <>
-        
+            <StyleFooter>
+                <h1>Contatos</h1>
+                <p><span>Email:</span> gabrielfurlaneti@gmail.com</p>
+                <a rel='noreferrer' href="https://github.com/DabGias" target={"_blank"}>🐙 GitHub 🐙</a>
+                <a rel='noreferrer' href="https://www.linkedin.com/in/gabrielfurlaneti" target={"_blank"}>👔 LinkedIn 👔</a>
+            </StyleFooter>
         </>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 const StyleMain = styled.main `
-    width: 70%;
+    width: 80%;
     margin: 20px auto;
 
     h1 {
@@ -22,6 +22,24 @@ const StyleMain = styled.main `
     h2 span {
         color: #57ad57;
     }
+
+    .info {
+        display: flex;
+        justify-content: space-between;
+        margin: 40px auto;
+        color: #d9d9d9;
+    }
+
+    .info h2 {
+        color: #627dff;
+        margin-bottom: 20px;
+    }
+
+    .info .hist p, .objtvs p {
+        margin: 0px 20px;
+        text-align: justify;
+        text-indent: 30px;
+    }
 `
 
 function Home() {
@@ -34,12 +52,12 @@ function Home() {
 
                 <section className="info">
                     <div className="hist">
-                        <h3>Minha história</h3>
+                        <h2>Minha história</h2>
 
-                        <p>Desde criança eu já possuia um certo contato com computadores, nada profundo, mas o suficinete para gerar interesse e uma certa habilidade. Durante minha adolecência mantive o contato com computadores, porém tinha um grande interesse e tirava boas notas em biologia, logo acreditei que essa era uma boa profissão, mas durante os últimos anos do meu ensino médio comecei um tecnólogo na área de manutenção e suporte em informática, e apesar do curso ser focado em hardware, o curso também abordava programação, porém após algumas semanas depois do inicio do tecnólogo a pandemia chegou e acabou com o curso que era totalmente baseado em prática e atividades laboratoriais, logo as únicas matérias que se mantiveram inalteradas foram as que abordavam programação, e nessas eu vi uma oportunidade de seguir a carreira que sigo hoje.</p>
+                        <p>Desde criança eu já possuia um certo contato com computadores, nada profundo, mas o suficinete para gerar interesse e uma certa habilidade. Durante minha adolecência mantive o contato com computadores, porém tinha um grande interesse e tirava boas notas em biologia, logo acreditei que essa era uma boa profissão, mas durante os últimos anos do meu ensino médio comecei um tecnólogo na área de manutenção e suporte em informática, e apesar do curso ser focado em hardware, o curso também abordava programação, porém após algumas semanas depois do início do tecnólogo a pandemia chegou e acabou com o curso que era totalmente baseado em prática e atividades laboratoriais, logo as únicas matérias que se mantiveram inalteradas foram as que abordavam programação, e nessas eu vi uma oportunidade de seguir a carreira que sigo hoje.</p>
                     </div>
                     <div className="objtvs">
-                        <h3>Meus objetivos</h3>
+                        <h2>Meus objetivos</h2>
 
                         <p>Meu principal objetivo é poder crescer como profissional e como pessoa, sempre aprendendo e buscando melhorar, mas também busco o equilíbrio, apesar de querer melhorar a todo momento, acredito que não se deve apenas seguir uma trilha sem se questionar ou analisar os seus arredores. No mercado de trabalho espero poder aplicar meus conhecimentos de forma consistente, e se cometer algums erros, aprender com eles. Também procuro ser dinâmico, mantendo o conhecimento em dia e a mente aberta para novas possibilidades de soluções ou abordagens.</p>
                     </div>
